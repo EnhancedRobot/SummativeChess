a034093763ab9518c3c5c8e6de28744c6839ea7c
EnhancedRobot <97986211+EnhancedRobot@users.noreply.github.com>
2025-12-18 20:54:15 -0500

    Added Capture any royal victory

---
+++ src/main/java/net/ics4u/summativechess/game/end/CaptureAnyRoyal.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.net.ics4u.summativechess.game.end;
+
+import main.java.net.ics4u.summativechess.game.board.Board;
+import main.java.net.ics4u.summativechess.game.pieces.base.Piece;
+
+/**
+ *
+ * @author joshu
+ */
+public class CaptureAnyRoyal extends VictoryCondition {
+
+    @Override
+    public VictoryState isEnded(Board board) {
+        // Create the list of who has lost
+        Boolean[] hasLost = new Boolean[board.numPlayers];
+        
+        // Set all of the hasLost to false
+        for(int i = 0; i < hasLost.length; i++) {
+            hasLost[i] = false;
+        }
+        
+        
+        // For every captured piece
+        for (Piece piece : board.capturedPieces) {
+            // If that piece is a king
+            if(piece.id.equals("K")) {
+                // Add it to the list of teams that have lost
+                hasLost[piece.player] = true;
+            }
+        }
+        
+        
+        return new VictoryState(hasLost);
+    }
+}
