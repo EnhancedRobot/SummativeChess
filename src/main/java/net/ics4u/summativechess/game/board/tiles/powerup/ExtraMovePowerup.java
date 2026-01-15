dba9d4f5f7033eba150541a886fec75feb2d1b5b
EnhancedRobot <97986211+EnhancedRobot@users.noreply.github.com>
2026-01-15 10:44:54 -0500

    Split PowerupTile into multiple classes

---
+++ src/main/java/net/ics4u/summativechess/game/board/tiles/powerup/ExtraMovePowerup.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.net.ics4u.summativechess.game.board.tiles.powerup;
+
+import main.java.net.ics4u.summativechess.game.board.tiles.Tile;
+import main.java.net.ics4u.summativechess.game.pieces.base.Piece;
+import main.java.net.ics4u.summativechess.game.pieces.moves.Move;
+import main.java.net.ics4u.summativechess.util.BoardPos;
+
+/**
+ *
+ * @author joshu
+ */
+public class ExtraMovePowerup extends Tile {
+    private boolean used = false; 
+
+    public ExtraMovePowerup(BoardPos pos) {
+        super();
+                
+        this.id = "PM";
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+    
+    @Override
+    public void onMoveTo(Move move) {    
+        // Activate the tile with the moving piece
+        activate(move.movingPiece);
+    }
+    
+    public void activate(Piece piece) {
+        // If the tile has been used, return
+        if(used) return;
+
+        // Set the piece to be able to move twice
+        // Unimplemented, because that's not how the engine works rn
+        piece.setExtraMove(true);
+        
+        // Set the tile to be used
+        used = true;
+    }
+}
---
+++ src/main/java/net/ics4u/summativechess/game/board/tiles/powerup/TeleportPowerup.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.net.ics4u.summativechess.game.board.tiles.powerup;
+
+import main.java.net.ics4u.summativechess.game.board.tiles.Tile;
+import main.java.net.ics4u.summativechess.game.pieces.base.Piece;
+import main.java.net.ics4u.summativechess.game.pieces.moves.Move;
+import main.java.net.ics4u.summativechess.util.BoardPos;
+
+/**
+ *
+ * @author joshu
+ */
+public class TeleportPowerup extends Tile {
+    private boolean used = false; 
+
+    public TeleportPowerup(BoardPos pos) {
+        super();
+                
+        this.id = "PT";
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+    
+    @Override
+    public void onMoveTo(Move move) {    
+        // Activate the tile with the moving piece
+        activate(move.movingPiece);
+    }
+    
+    public void activate(Piece piece) {
+        // If the tile has been used, return
+        if(used) return;
+
+        // Set the piece to be able to teleport
+        piece.setCanTeleport(true);
+        
+        // Set the tile to be used
+        used = true;
+    }
+}
